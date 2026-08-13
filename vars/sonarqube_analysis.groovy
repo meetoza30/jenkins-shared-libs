f875d58f04eb7e798588fe5866b53afa0bccc00b
Meet Oza <meetoz305@gmail.com>
2026-08-13 14:26:29 +0530

    sonar name updated

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -1,6 +1,6 @@
 def call(String sonarEnv, String projName, String projKey) {
     
-    def scannerHome = tool 'SonarQubeScanner'
+    def scannerHome = tool 'SONAR'
     
     withSonarQubeEnv(sonarEnv) {
         sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectName='${projName}' -Dsonar.projectKey='${projKey}'"
