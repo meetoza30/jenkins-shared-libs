80cddaa3aeada1d433cd1a5205fb8fc4ecc558d4
Meet Oza <meetoz305@gmail.com>
2026-08-14 22:43:18 +0530

    owasp groovy update

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,6 +1,6 @@
 def call(){
 	withCredentials([string(credentialsId: 'nvd-api-key', variable: 'NVD_KEY')]){
-	dependencyCheck additionalArguments: "--scan ./ --nvdApiKey ${NVD_KEY} --autoUpdate false", odcInstallation: 'OWASP'
+	dependencyCheck additionalArguments: "--scan ./ --nvdApiKey ${NVD_KEY}", odcInstallation: 'OWASP'
 	dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
 }
 }
